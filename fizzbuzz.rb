efc3f2ae3a3d59c7533b8f83d039a2f392871eb8
Sonia  Nicas <sduarte22@gmail.com>
2018-04-14 21:37:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
      return "Fizz"
   end  
    
-  else if number % 5 == 0  
+  elsif number % 5 == 0  
      return "Buzz"
   end
        
-  else if number % 5 == 0 && number % 3 == 0  
+  elsif number % 5 == 0 && number % 3 == 0  
      return "FizzBuzz"
   end    
     
