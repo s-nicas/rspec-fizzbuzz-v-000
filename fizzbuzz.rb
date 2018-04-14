f5b6565d5bc5e4d85e4c955356aefddcb19a2ae6
Sonia  Nicas <sduarte22@gmail.com>
2018-04-14 21:28:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,11 @@
 def fizzbuzz(number)
-  if number % 3==0
+  if number % 3== 0
+    return "Fizz"
+  else if number % 5 == 0  
+    return "Buzz"
+  else if number % 5 == 0 && number % 3 == 0  
+    return "FizzBuzz"
+  else 
+    return nil
   
 end  
