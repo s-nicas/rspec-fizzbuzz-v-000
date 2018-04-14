7f3b693ca7300fb431ebb4166296c60d7c8e742b
Sonia  Nicas <sduarte22@gmail.com>
2018-04-14 21:39:39 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
  def fizzbuzz(number)
   if number % 3== 0
      return "Fizz"
-  end  
+  
    
   elsif number % 5 == 0  
      return "Buzz"
-  end
+  
        
   elsif number % 5 == 0 && number % 3 == 0  
      return "FizzBuzz"
-  end    
+      
     
   else 
     return  nil 
